2885e977452eb73d8c22b54ab037db01372f6beb
Haris Barki <harisbarki@ymail.com>
2017-03-17 00:50:04 -0400

    initial commit

---
+++ src/app/components/search/search.module.ts
@@ -0,0 +1,27 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+
+import {AppComponent} from './app.component';
+import {SearchComponent} from './components/search/search.component';
+import {AboutComponent} from './components/about/about.component';
+import {NavbarComponent} from './components/navbar/navbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SearchComponent,
+    AboutComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
